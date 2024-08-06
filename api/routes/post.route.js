a5c6fbdc3ed5c06157ab3ea2ecd0fc015e32f0f4
benshabbat <benshabbat27@gmail.com>
2024-08-06 12:44:18 +0300

    without logic just structure

---
+++ api/routes/post.route.js
@@ -0,0 +1,18 @@
+import express from "express";
+
+const router = express.Router();
+
+router.get("/",(req,res)=>{
+    
+})
+router.post("/",(req,res)=>{
+    
+})
+router.put("/",(req,res)=>{
+    
+})
+router.delete("/",(req,res)=>{
+    
+})
+
+export default router;
