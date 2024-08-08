55b2ec75055dde38879c08fd1b5e43349cf19031
benshabbat <benshabbat27@gmail.com>
2024-08-08 13:17:57 +0300

    hashedPassword

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,11 @@
 import bcrypt from "bcrypt";
 
-export const register = (req, res) => {
-  console.log(req.body);
+export const register = async(req, res) => {
+  // console.log(req.body);
+  const{username,email,password}=req.body;
+
+  const hashedPassword = await bcrypt.hash(password,12);
+  console.log(hashedPassword);
 };
 export const login = (req, res) => {};
 export const logout = (req, res) => {};
