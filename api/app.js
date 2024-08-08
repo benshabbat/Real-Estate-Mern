0674990bd24c1d7a60efe5b02cdedd4c768fcbef
benshabbat <benshabbat27@gmail.com>
2024-08-08 11:20:45 +0300

    install bcrypt

--- api/app.js
+++ api/app.js
@@ -4,5 +4,5 @@ import postRoute from "./routes/post.route"
 const app = express();
 
 app.use("/api/auth", authRoute);
-app.use("/api/posts", authRoute);
+app.use("/api/posts", postRoute);
 app.listen(8800,() => {console.log("Server listening on")});
